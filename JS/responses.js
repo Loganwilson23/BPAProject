86f0091cead3b50e5953e914d8c7c772db4e65da
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-10-11 13:14:19 -0400

    Add files via upload

---
+++ JS/responses.js
@@ -0,0 +1,31 @@
+function getBotResponse(input) {
+  // buying a car repsonses
+  if (input == "How do i buy a car?") {
+    return "You would go to the home page or go to our sales page";
+} else if (input == "how do i buy a car?") {
+    return "You would go to the home page or go to our sales page";
+} else if (input == "where can i buy a car at") {
+    return "You would go to the home page or go to our sales page";
+}
+
+// Simple responses
+if (input == "hello") {
+    return "Hello there!";
+} else if (input == "goodbye") {
+    return "Talk to you later!";
+} else if(input == "Good"){
+    return "That is good , How may I help you today ?" ;
+}
+else if(input == "good"){
+    return "That is good , How may I help you today ?" ;
+}
+else if(input == "Bad"){
+    return "awe I'm sorry to hear that , How may I help you today ?" ;
+}
+else if(input == "bad"){
+    return "awe I'm sorry to hear that  , How may I help you today ?" ;
+}
+else {
+    return "Try asking something else!";
+}
+}
