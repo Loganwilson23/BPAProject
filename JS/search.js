c21a014382870bda114a9bb8fa24bbbb71bf5a43
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-10-18 14:03:54 -0400

    Search bar little updates

--- JS/search.js
+++ JS/search.js
@@ -1,6 +1,22 @@
+import React from "react";
+import SearchBar from "./Components/SearchBar";
+import BookData from "./Data.json";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar placeholder="Enter a Book Name..." data={BookData} />
+    </div>
+  );
+}
+
+export default App;
+
 const searchInput = document.querySelector("[data-search]")
 
 searchInput.addEventListener("input", (e) => {
     const value = e.target.value.toLowerCase()
     console.log(value)
 })
+
+
