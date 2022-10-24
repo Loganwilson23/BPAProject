57d0fb381b6df68ae4fe1efe381877bdf84c62f0
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-10-24 11:26:21 -0400

    New nar bar and footer fixes

--- JS/search.js
+++ JS/search.js
@@ -1,13 +1,5 @@
-import React from "react";
-import SearchBar from "./Components/SearchBar";
-import BookData from "Data.json";
-
 function App() {
-  return (
-    <div className="App">
-      <SearchBar placeholder="Enter a Book Name..." data={BookData} />
-    </div>
-  );
+
 }
 
 export default App;
