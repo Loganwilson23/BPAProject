fc0b0cdf7931653362b26024f42378fc459fe9dc
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-10-25 11:28:25 -0400

    navbar and footer is done for css

--- JS/search.js
+++ JS/search.js
@@ -1,14 +1,2 @@
-function App() {
-
-}
-
-export default App;
-
-const searchInput = document.querySelector("[data-search]")
-
-searchInput.addEventListener("input", (e) => {
-    const value = e.target.value.toLowerCase()
-    console.log(value)
-})
 
 
