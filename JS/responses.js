4f73afedb0b6e9a6c3c0e72be1aef178aa2b6094
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-11-02 13:36:45 -0400

    fixed a few things

--- JS/responses.js
+++ JS/responses.js
@@ -1,38 +1,38 @@
 function getBotResponse(input) {
   // buying a car repsonses
-  if (input.toLowercase == "How do i buy a car?") {
+  if (input == "How do i buy a car?") {
     return "You would go to the home page or go to our sales page";
-} else if (input.toLowercase == "how do i buy a car?") {
+} else if (input == "how do i buy a car?") {
     return "You would go to the home page or go to our sales page";
-} else if (input.toLowercase == "where can i buy a car at") {
+} else if (input == "where can i buy a car at") {
     return "You would go to the home page or go to our sales page";
 }
-else if (input.toLowercase == "how can i buy a car?") {
+else if (input == "how can i buy a car?") {
     return "You would go to the home page or go to our sales page";
 }
 
 // Simple responses
-if (input.toLowercase == "hello") {
+if (input == "hello") {
     return "Hello there!";
-} else if (input.toLowercase == "goodbye") {
+} else if (input == "goodbye") {
     return "Talk to you later!";
-} else if(input.toLowercase == "Good"){
+} else if(input == "Good"){
     return "That is good , How may I help you today ?" ;
 }
 else if(input == "good"){
     return "That is good , How may I help you today ?" ;
 }
-else if(input.toLowercase == "Bad"){
+else if(input == "Bad"){
     return "awe I'm sorry to hear that , How may I help you today ?" ;
 }
-else if(input.toLowercase == "bad"){
+else if(input == "bad"){
     return "awe I'm sorry to hear that  , How may I help you today ?" ;
 }
 
 
 
 // finding pages
-if (input.toLowercase == "where can i find about Us page") {
+if (input == "where can i find about us page") {
   return "You can find the About Us page in the top left of the page saying About US" ;
 } else if (input.toLowercase == "goodbye") {
     return "Talk to you later!";
