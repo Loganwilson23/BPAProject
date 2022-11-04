9901a447ecc8a5982b79a6972d5db9a042c720f3
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-11-04 14:05:18 -0400

    added new colors and carsel

--- JS/searchbar.js
+++ JS/searchbar.js
@@ -1,61 +1,81 @@
-import React, { useState } from "react";
-import "./SearchBar.css";
-import SearchIcon from "@material-ui/icons/Search";
-import CloseIcon from "@material-ui/icons/Close";
-
-function SearchBar({ placeholder, data }) {
-  const [filteredData, setFilteredData] = useState([]);
-  const [wordEntered, setWordEntered] = useState("");
-
-  const handleFilter = (event) => {
-    const searchWord = event.target.value;
-    setWordEntered(searchWord);
-    const newFilter = data.filter((value) => {
-      return value.title.toLowerCase().includes(searchWord.toLowerCase());
-    });
-
-    if (searchWord === "") {
-      setFilteredData([]);
-    } else {
-      setFilteredData(newFilter);
-    }
+//shortens document.getEgetElementById
+function element(id) {
+  return document.getElementById(id);
+  }
+  let allSearchData = ""; //decleared to collect all search names
+  
+  //gets each inputs data starting from second input
+  function getResults() {
+  //gets value of input
+  let search = element("search-input").value;
+  allSearchData = ""; //clears data for each word typed
+  
+  hideSearchResults();
+  clearSearchResults();
+  clearSearchData(); //
+  //starts searching from the second input
+  if (search.length > 1) {
+      let counter = 0; // counts to 10
+      for (let x of names) {
+      if (counter < 10) {
+          //checks for similarities
+          if (x.toLowerCase().includes(search.toLowerCase())) {
+          //populates the suggestion div
+          element("search-results").innerHTML +=
+              "<div class='search-item' onclick='displayData(\"" +
+              x +
+              "\")'><p>" +
+              x +
+              "</p></div>";
+  
+          counter++;
+          }
+      }
+      if (x.toLowerCase().includes(search.toLowerCase()))
+          //saves all the realated names
+          allSearchData += "<p>" + x + "</p>";
+      }
+      displaySearchResults();
+  }
+  }
+  //displays the suggestion div
+  function displaySearchResults() {
+  element("search-results").style.display = "block";
+  }
+  //clears the suggestion div
+  function clearSearchResults() {
+  element("search-results").innerHTML = "";
+  }
+  
+  //hides the suggestion div
+  function hideSearchResults() {
+  element("search-results").style.display = "none";
+  }
+  //displays names when you click a suggestions
+  function displayData(name) {
+  element("search-data").innerHTML = "<p>" + name + "</p>";
+  hideSearchResults();
+  }
+  //displays all related names to your search when you hit enter
+  function displayAllData(names) {
+  element("search-data").innerHTML = names;
+  hideSearchResults();
+  }
+  //clears names displayed from search result
+  function clearSearchData() {
+  element("search-data").innerHTML = "";
+  }
+  //gets results after each input
+  element("search-input").oninput = function() {
+  getResults();
   };
-
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
-  };
-
-  return (
-    <div className="search">
-      <div className="searchInputs">
-        <input
-          type="text"
-          placeholder={placeholder}
-          value={wordEntered}
-          onChange={handleFilter}
-        />
-        <div className="searchIcon">
-          {filteredData.length === 0 ? (
-            <SearchIcon />
-          ) : (
-            <CloseIcon id="clearBtn" onClick={clearInput} />
-          )}
-        </div>
-      </div>
-      {filteredData.length != 0 && (
-        <div className="dataResult">
-          {filteredData.slice(0, 15).map((value, key) => {
-            return (
-              <a className="dataItem" href={value.link} target="_blank">
-                <p>{value.title} </p>
-              </a>
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default SearchBar;
+  
+  element("search-input").addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      displayAllData(allSearchData);
+  }
+  });
