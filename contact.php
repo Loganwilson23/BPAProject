cb5ad0c9219fc8ee312aa1bbf1f0225c9f84ae05
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-12-13 14:00:42 -0500

    added contact page

---
+++ contact.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+Welcome <?php echo $_POST['fname']; ?>
+ <?php echo $_POST['lname']; ?>
+ <br>
+ <?php echo $_POST['country']; ?>
+<br>
+<?php echo $_POST['subject']; ?>
+</body>
+</html>
