8e55cc9b9df5141c1a3f068fa6f6d9c737e0f3e5
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-12-14 13:48:14 -0500

    fixed some repsonses

--- JS/responses.js
+++ JS/responses.js
@@ -1,49 +1,50 @@
 function getBotResponse(input) {
-  // buying a car reponses
-  if (input == "How do i buy a car?") {
-    return "You would go to the home page or go to our sales page";
-} else if (input == "how do i buy a car?") {
-    return "You would go to the home page or go to our sales page";
-} else if (input == "where can i buy a car at") {
-    return "You would go to the home page or go to our sales page";
-}
-else if (input == "how can i buy a car?") {
-    return "You would go to the home page or go to our sales page";
-}
+    // buying a car reponses
+    console.log(input)
+    if (input == "How do i buy a car?") {
+        return "You would go to the home page or go to our sales page";
+    } else if (input == "how do i buy a car?") {
+        return "You would go to the home page or go to our sales page";
+    } else if (input == "where can i buy a car at") {
+        return "You would go to the home page or go to our sales page";
+    }
+    else if (input == "how can i buy a car?") {
+        return "You would go to the home page or go to our sales page";
+    }
 
-// Simple responses
-if (input == "hello") {
-    return "Hello there!";
-} else if (input == "goodbye") {
-    return "Talk to you later!";
-} else if(input == "good"){
-    return "That is good , How may I help you today ?" ;
-}
-else if(input == "bad"){
-    return "awe I'm sorry to hear that  , How may I help you today ?" ;
-}
+    // Simple responses
+    if (input == "hello" || input == "Hello") {
+        return "Hello there! How can I help you today? ";
+    } else if (input == "goodbye" || input == "Goodbye") {
+        return "Talk to you later!";
+    } else if (input == "good" || input == "Good") {
+        return "That is good , How may I help you today ?";
+    }
+    else if (input == "bad" || input == "Bad") {
+        return "awe I'm sorry to hear that  , How may I help you today ?";
+    }
 
 
 
-// finding pages
-if (input == "where can i find about us page") {
-  return "You can find the About Us page in the top left of the page saying About US" ;
-} else if (input.toLowercase == "blue") {
-    return "Talk to you later!";
-} else if(input.toLowercase == "red"){
-    return "That is good , How may I help you today ?" ;
-}
-else if(input.toLowercase == "green"){
-    return "That is good , How may I help you today ?" ;
-}
-else if(input.toLowercase == "purple"){
-    return "awe I'm sorry to hear that , How may I help you today ?" ;
-}
-else if(input.toLowercase == "black"){
-    return "awe I'm sorry to hear that  , How may I help you today ?" ;
-}
-else {
-    return "Try asking something else!";
-}
+    // finding pages
+    if (input == "where can i find about us page") {
+        return "You can find the About Us page in the top left of the page saying About US";
+    } else if (input == "blue" || input == "Blue") {
+        return "Talk to you later!";
+    } else if (input == "red" || input == "Red") {
+        return "That is good , How may I help you today ?";
+    }
+    else if (input == "green" || input == "Green") {
+        return "That is good , How may I help you today ?";
+    }
+    else if (input == "purple" || input == "Purple") {
+        return "awe I'm sorry to hear that , How may I help you today ?";
+    }
+    else if (input == "black" || input == "Black") {
+        return "awe I'm sorry to hear that  , How may I help you today ?";
+    }
+    else {
+        return "Try asking something else!" + " (Like car colors: How  do I buy a car)";
+    }
 
 }
