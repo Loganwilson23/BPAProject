9a786af25747d58626dfd0facd5d6b6113c3839a
Loganwilson23 <91540252+Loganwilson23@users.noreply.github.com>
2022-12-15 13:57:43 -0500

    added more responses

--- JS/responses.js
+++ JS/responses.js
@@ -1,17 +1,6 @@
 function getBotResponse(input) {
     // buying a car reponses
     console.log(input)
-    if (input == "How do i buy a car?") {
-        return "You would go to the home page or go to our sales page";
-    } else if (input == "how do i buy a car?") {
-        return "You would go to the home page or go to our sales page";
-    } else if (input == "where can i buy a car at") {
-        return "You would go to the home page or go to our sales page";
-    }
-    else if (input == "how can i buy a car?") {
-        return "You would go to the home page or go to our sales page";
-    }
-
     // Simple responses
     if (input == "hello" || input == "Hello") {
         return "Hello there! How can I help you today? ";
@@ -24,25 +13,41 @@ function getBotResponse(input) {
         return "awe I'm sorry to hear that  , How may I help you today ?";
     }
 
-
-
-    // finding pages
-    if (input == "where can i find about us page") {
+//  questions
+   else if (input == "How do i buy a car?" || input == "how do i buy a car?") {
+        return "You would go to the home page or go to our sales page";
+    } else if (input == "where can i buy a car at" || input== "Where can i buy a car at") {
+        return "You would go to the home page or go to our sales page";
+    }
+    else if (input == "where can i find about us page") {
         return "You can find the About Us page in the top left of the page saying About US";
     } else if (input == "blue" || input == "Blue") {
-        return "Talk to you later!";
+        return "We have ";
     } else if (input == "red" || input == "Red") {
-        return "That is good , How may I help you today ?";
+        return "We have  of this color car";
     }
     else if (input == "green" || input == "Green") {
-        return "That is good , How may I help you today ?";
+        return "We have  of this color car";
     }
     else if (input == "purple" || input == "Purple") {
-        return "awe I'm sorry to hear that , How may I help you today ?";
+        return "We have  of this color car";
     }
     else if (input == "black" || input == "Black") {
-        return "awe I'm sorry to hear that  , How may I help you today ?";
+        return "We have  of this color car";
     }
+    else if (input == "Car Colors" || input == "car colors") {
+        return "We currently have Red,Blue,Green,Purple and black color cars";
+    }
+    else if (input == "What kind of cars do you guys have?" || input == "what kind of cars do you guys have?") {
+        return "We currently have Ford,Tesla,Mercedes,BMW, Mazda,Toyota,Honda,Range Rover,Lincoln,Audi,Subaru,Ram,Lexus,Volvo,";
+    } 
+     else if (input == "how do i use the finace calculator " || input == "How do i use the finace calculator") {
+        return "for the Amount (type in your loan amount), for interest rate(type in your interest rate of the loan), for the Monthly amount(type in how many months you would like to finace the loan)";
+    }
+    
+
+
+
     else {
         return "Try asking something else!" + " (Like car colors: How  do I buy a car)";
     }
